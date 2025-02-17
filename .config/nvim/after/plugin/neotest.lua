10e270f8e2d99728729cd27d1988dfae4fce55f9
Maël Simon <simonmaeldev@gmail.com>
2025-02-17 16:17:23 -0500

    better tests

--- .config/nvim/after/plugin/neotest.lua
+++ .config/nvim/after/plugin/neotest.lua
@@ -1,7 +1,38 @@
 require("neotest").setup({
   adapters = {
-    require("neotest-python")
-  }
+    require("neotest-python"),
+  },
+  -- Enable floating window for test output
+  floating = {
+    border = "rounded",
+    max_height = 0.9,
+    max_width = 0.9,
+  },
+  -- Enable statusline integration
+  status = {
+    enabled = true,
+    signs = true,
+    virtual_text = false,
+  },
 })
 
-vim.keymap.set("n", "<leader>ts", function() require("neotest").summary.toggle() end)
+-- Add more comprehensive keymaps for testing
+vim.keymap.set('n', '<leader>tn', function() 
+  require('neotest').run.run() 
+end, { desc = 'Run nearest test' })
+
+vim.keymap.set('n', '<leader>tf', function() 
+  require('neotest').run.run(vim.fn.expand('%')) 
+end, { desc = 'Run current file tests' })
+
+vim.keymap.set('n', '<leader>ts', function() 
+  require('neotest').summary.toggle() 
+end, { desc = 'Toggle test summary' })
+
+vim.keymap.set('n', '<leader>td', function()
+  require('neotest').run.run({ strategy = "dap" })
+end, { desc = 'Debug nearest test' })
+
+vim.keymap.set('n', '<leader>to', function()
+  require('neotest').output.open({ enter = true })
+end, { desc = 'Open test output' })
